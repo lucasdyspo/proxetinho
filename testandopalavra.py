a90bba84e99e12dbec2b482eeebef427ba46d9ab
lucasdyspo <80234431+lucasdyspo@users.noreply.github.com>
2022-02-07 19:20:49 -0300

    Add files via upload

--- testandopalavra.py
+++ testandopalavra.py
@@ -14,6 +14,8 @@ print(tokens_musica.get('volume'))
 
 dic_funcoes = ["0, 1, pause(), play(), 4, escolher_volume(), resume()", "23, 23", "dgds", "mana", "melao"]
 class interprete:
+
+
     def interpretar_texto(self):
         n1 = []
         n1_1 = []
@@ -21,35 +23,58 @@ class interprete:
             if palavra in palavraschave:
                 n1_1.append(palavra)
                 n1.append(palavraschave.index(palavra))
-            else:
-                pass
-        return (n1)
 
-    def interpretar_texto2(self):
-        n1 = []
-        n1_1 = []
-        for palavra in self:
-            if palavra in palavraschave:
-                n1_1.append(palavra)
-                n1.append(palavraschave.index(palavra))
             else:
                 pass
-        return (n1_1)
+        return (n1, n1_1)
+
 
 
 
     def interpretar_modulo(self):
+        lista = self[0]
         modulo = []
-        for i in self:
+        for i in lista:
             if i > 0 and i < 10:
                 modulo.append(tokens_social)
             elif i > 11  and  i < 25:
                 modulo.append(tokens_musica)
-                break
-        return (modulo[0])
+
+        return modulo
 
     def interpretar_modulo2(self, modulo):
-        pass
+        for i in modulo:
+            if modulo.count(i) > 2:
+                return i
+            else:
+                pass
+
+        modulo.reverse()
+
+        return modulo[0]
+
+
+    t = (interprete.interpretar_modulo((interprete.interpretar_texto(u))))
+    mod_dict = (interprete.interpretar_modulo2(0, t))
+        # if type(mod_dict) = None:
+        #     modulo_ativo := False
+        # else:
+        #     modulo_ativo := True
+
+
+
+
+class selecionar(interprete):
+    def __init__(self):
+        global modulo_ativo
+        if modulo_ativo := False:
+            n1.clear()
+            n1_1.clear()
+            modulo.clear()
+            n2.clear()
+        else:
+            pass
+
 
     def interpretar_atividade(self, palavras_ditas, lista_tokens):
         n2 = []
@@ -57,22 +82,24 @@ class interprete:
         for palavra_chave in palavras_ditas:
             if palavra_chave in lista_tokens:
                 n2.append(lista_tokens.get(str(palavra_chave)))
-                print("7")
             else:
                 n3.append(lista_tokens.get(palavra_chave))
         return (n2)
 
-def selecionar_atividade(funcoes):
-    atividades_lista = []
-    lista_tokens_atividade = funcoes
-    numero = len(lista_tokens_atividade)
-    for i in range(numero):
-        atividades_lista.append(dic_funcoes[(lista_tokens_atividade[i])])
-
-    return (atividades_lista)
 
 
+    def selecionar_atividade(self, funcoes):
+        atividades_lista = []
+        lista_tokens_atividade = funcoes
+        numero = len(lista_tokens_atividade)
+        for i in range(numero):
+            atividades_lista.append(dic_funcoes[(lista_tokens_atividade[i])])
 
+        return (atividades_lista)
+
+
+
+    lista_atividade = selecionar_atividade(interpretar_atividade(u, mod_dict))
 
 
 
