a42a987c75e20ecd2d06ccdf7d3eff7c14e68d07
lucasdyspo <80234431+lucasdyspo@users.noreply.github.com>
2022-03-28 18:03:35 -0300

    Add files via upload

--- nutricao.py
+++ nutricao.py
@@ -1,33 +1,38 @@
 import requests
 import json
 
-alimentacao_objetivo = {"gordura_ideal" : 580,
-"proteina_ideal" : 65,
-"carbo_ideal" : 260,
-"calorias_ideal" : 2680}
 
 
-class alimentacao:
-    def pesquisa_alimento (self, alimento):
-        nutrientes_alimento = []
-        # gramas =  (gramas input)/100
-        dados_alimentos = (requests.get(f"https://api.edamam.com/api/food-database/v2/parser?app_id=147a6cdf&app_key=151a5a0252dfd4b3f9dbbe47fd7c21dc&ingr={alimento}&nutrition-type=cooking"))
+
+class nutricao:
+
+    fat_ideal = 580
+    protein_ideal = 65
+    carbo_ideal = 260
+    caloria_ideal = 2680
+
+
+    @classmethod
+    def modify_meta_nutrients(cls, period, fat = fat_ideal, protein = protein_ideal, carbo = carbo_ideal, caloria = caloria_ideal):
+        pass
+
+
+class alimentacao(nutricao):
+    def __init__(self, food, gramas):
+        self. gramas = gramas / 100
+        dados_alimentos = (requests.get(f"https://api.edamam.com/api/food-database/v2/parser?app_id=147a6cdf&app_key=151a5a0252dfd4b3f9dbbe47fd7c21dc&ingr={food}&nutrition-type=cooking"))
         tabela = dados_alimentos.json()
         tabela_nutricional = gramas * (tabela['parsed'][0]['food']['nutrients'])
-        nutrientes_alimento.append(gramas * (tabela_nutricional["ENERC_KCAL"]))
-        nutrientes_alimento.append(gramas * (tabela_nutricional['PROCNT']))
-        nutrientes_alimento.append(gramas * (tabela_nutricional['FAT']))
-        nutrientes_alimento.append(gramas * (tabela_nutricional['CHOCDF']))
-        nutrientes = ['caloria', 'proteina', 'gordura', 'carbo', 'dia']
-        return (nutrientes, nutrientes_alimento)
-
+        self.calorie = (gramas * (tabela_nutricional["ENERC_KCAL"]))
+        self.protein = (gramas * (tabela_nutricional['PROCNT']))
+        self.fat = (gramas * (tabela_nutricional['FAT']))
+        self.carbohydrate = (gramas * (tabela_nutricional['CHOCDF']))
 
 
 def registro_proxetinho():
     con = mysql.connector.connect(host='localhost', database='proxetinho', user='root', password='Lucashaha10*')
     if con.is_connected():
-        # colunas =
-        # valores =
+
 
         for coluna in colunas:
             index = colunas.index(coluna)
